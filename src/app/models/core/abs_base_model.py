c69ac006e876d501e19dbc73779074e156041a52
mateuskolap <mateuskolap@gmail.com>
2025-08-17 19:02:12 -0300

    feat: adiciona colunas 'created_by' e 'updated_by' na tabela base

--- src/app/models/core/abs_base_model.py
+++ src/app/models/core/abs_base_model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, func
+from sqlalchemy import ForeignKey, Integer, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.app.models.core.registry import table_registry
@@ -11,6 +11,12 @@ class AbsBaseModel:
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
+    created_by: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), nullable=True, init=False
+    )
+    updated_by: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), nullable=True, init=False
+    )
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), nullable=False, init=False
     )
